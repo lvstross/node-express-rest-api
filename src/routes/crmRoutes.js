48fc8301a7c5641e14b34ce85c531776d79f1b31
Levi Gonzales <lvstross@gmail.com>
2020-01-22 12:41:41 -0700

    checked some middleware in routes.

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,6 +1,11 @@
 const routes = (app) => {
   app.route('/contact')
-    .get((req, res) => {
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    }, (req, res, next) => {
       res.send('GET request successful!');
     })
     .post((req, res) => {
